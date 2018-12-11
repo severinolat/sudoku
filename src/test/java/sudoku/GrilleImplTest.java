0dc0131c61488171ba3042bee084da3b275b8ee1
severinolat <severinlawson@gmail.com>
2018-12-11 10:48:10 +0000

    Update GrilleImplTest.java

--- src/test/java/sudoku/GrilleImplTest.java
+++ src/test/java/sudoku/GrilleImplTest.java
@@ -18,98 +18,167 @@ import static org.junit.Assert.*;
  */
 public class GrilleImplTest {
     
-    public GrilleImplTest() {
+        public GrilleImplTest() {
     }
     
-   
-
+    
+    
+    
     /**
-     * Test of main method, of class GrilleImpl.
+     * Test de la methode main de  GrilleImpl.
      */
     @Test
     public void testMain() {
         System.out.println("main");
         String[] args = null;
         GrilleImpl.main(args);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+       
     }
 
     /**
-     * Test of getDimension method, of class GrilleImpl.
+     * Test de la methode getdimension class GrilleImpl.
      */
     @Test
     public void testGetDimension() {
         System.out.println("getDimension");
-        GrilleImpl instance = null;
-        int expResult = 0;
+   
+        char[][] GRID_TO_SOLVE = {
+			{'9','0','0','1','0','0','0','0','5'},
+                        {'0','0','5','0','9','0','2','0','1'},
+                        {'8','0','0','0','4','0','0','0','0'},
+                        {'0','0','0','0','8','0','0','0','0'},
+                        {'0','0','0','7','0','0','0','0','0'},
+                        {'0','0','0','0','2','6','0','0','9'},
+                        {'2','0','0','3','0','0','0','0','6'},
+                        {'0','0','0','2','0','0','9','0','0'},
+                        {'0','0','1','9','0','4','5','7','0'},
+	};
+        
+        
+        GrilleImpl instance = new GrilleImpl(GRID_TO_SOLVE);
+        int expResult = 9;
         int result = instance.getDimension();
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        
     }
 
     /**
-     * Test of setValue method, of class GrilleImpl.
+     * Test de la methode testsetvalue GrilleImpl.
      */
     @Test
     public void testSetValue() {
         System.out.println("setValue");
+        
+         char[][] GRID_TO_SOLVE = {
+			{'9','0','0','1','0','0','0','0','5'},
+                        {'0','0','5','0','9','0','2','0','1'},
+                        {'8','0','0','0','4','0','0','0','0'},
+                        {'0','0','0','0','8','0','0','0','0'},
+                        {'0','0','0','7','0','0','0','0','0'},
+                        {'0','0','0','0','2','6','0','0','9'},
+                        {'2','0','0','3','0','0','0','0','6'},
+                        {'0','0','0','2','0','0','9','0','0'},
+                        {'0','0','1','9','0','4','5','7','0'},
+	};
+             
+        GrilleImpl instance = new GrilleImpl(GRID_TO_SOLVE);
+      
         int x = 0;
         int y = 0;
-        char value = ' ';
-        GrilleImpl instance = null;
-        instance.setValue(x, y, value);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        char value = '2';
+       
+    instance.setValue(x, y, value);
+         
     }
 
     /**
-     * Test of getValue method, of class GrilleImpl.
+     * Test de la methode getValue de la class GrilleImpl.
      */
     @Test
     public void testGetValue() {
         System.out.println("getValue");
-        int x = 0;
-        int y = 0;
-        GrilleImpl instance = null;
-        char expResult = ' ';
+        
+         char[][] GRID_TO_SOLVE = {
+			{'9','0','0','1','0','0','0','0','5'},
+                        {'0','0','5','0','9','0','2','0','1'},
+                        {'8','0','0','0','4','0','0','0','0'},
+                        {'0','0','0','0','8','0','0','0','0'},
+                        {'0','0','0','7','0','0','0','0','0'},
+                        {'0','0','0','0','2','6','0','0','9'},
+                        {'2','0','0','3','0','0','0','0','6'},
+                        {'0','0','0','2','0','0','9','0','0'},
+                        {'0','0','1','9','0','4','5','7','0'},
+	};
+       
+          
+        GrilleImpl instance = new GrilleImpl(GRID_TO_SOLVE);
+        
+        int x = 1;
+        int y = 1;
+      
+        char expResult = '0';
         char result = instance.getValue(x, y);
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        
     }
 
     /**
-     * Test of complete method, of class GrilleImpl.
+     * Test de la methode complete de la class GrilleImpl.
      */
     @Test
     public void testComplete() {
         System.out.println("complete");
-        GrilleImpl instance = null;
-        boolean expResult = false;
+        char[][] GRID_TO_SOLVE = {
+			{'9','0','0','1','0','0','0','0','5'},
+                        {'0','0','5','0','9','0','2','0','1'},
+                        {'8','0','0','0','4','0','0','0','0'},
+                        {'0','0','0','0','8','0','0','0','0'},
+                        {'0','0','0','7','0','0','0','0','0'},
+                        {'0','0','0','0','2','6','0','0','9'},
+                        {'2','0','0','3','0','0','0','0','6'},
+                        {'0','0','0','2','0','0','9','0','0'},
+                        {'0','0','1','9','0','4','5','7','0'},
+	};
+          
+        GrilleImpl instance = new GrilleImpl(GRID_TO_SOLVE);
+        boolean expResult = true;
         boolean result = instance.complete();
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        
     }
-
-  
     /**
-     * Test of possible method, of class GrilleImpl.
+     * Test de la methode possible de la class GrilleImpl.
      */
     @Test
     public void testPossible() {
         System.out.println("possible");
-        int x = 0;
-        int y = 0;
+        
+          char[][] GRID_TO_SOLVE = {
+			{'9','0','0','1','0','0','0','0','5'},
+                        {'0','0','5','0','9','0','2','0','1'},
+                        {'8','0','0','0','4','0','0','0','0'},
+                        {'0','0','0','0','8','0','0','0','0'},
+                        {'0','0','0','7','0','0','0','0','0'},
+                        {'0','0','0','0','2','6','0','0','9'},
+                        {'2','0','0','3','0','0','0','0','6'},
+                        {'0','0','0','2','0','0','9','0','0'},
+                        {'0','0','1','9','0','4','5','7','0'},
+	};
+       
+          
+        GrilleImpl instance = new GrilleImpl(GRID_TO_SOLVE);
+        
+        
+        
+        int x = 1;
+        int y = 1;
         char value = ' ';
-        GrilleImpl instance = null;
+      
         boolean expResult = false;
-        boolean result = instance.POSSIBLEc(x, y, value);
+        boolean result = instance.possible(x, y, value);
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        
     }
+
     
 }
